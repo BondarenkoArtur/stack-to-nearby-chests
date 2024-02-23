82c4a77360fb0137361cd64a60650ce74cda3365
Artur Bondarenko <uabartgamer@gmail.com>
2024-02-23 14:43:20 -0500

    Update EndWorldTickExecutor.java
    Another random try to fix

--- src/main/java/io/github/xiaocihua/stacktonearbychests/EndWorldTickExecutor.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/EndWorldTickExecutor.java
@@ -1,6 +1,8 @@
 package io.github.xiaocihua.stacktonearbychests;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.screen.PlayerScreenHandler;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -12,6 +14,10 @@ public class EndWorldTickExecutor {
     public static void init() {
         ClientTickEvents.END_WORLD_TICK.register(world -> {
             while (tasks.peek() != null) {
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.player != null && client.currentScreen instanceof PlayerScreenHandler) {
+                    client.player.closeHandledScreen(); // This might fix issue with ChestTracker
+                }
                 tasks.poll().run();
             }
         });
